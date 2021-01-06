dfa8063e9b15901be13edbe325e8559060b208b8
Davide Pacco <72199035+HolaSoyGonzalo@users.noreply.github.com>
2021-01-06 15:10:43 -0800

    pdf (not working)

--- src/services/cart/index.js
+++ src/services/cart/index.js
@@ -38,6 +38,36 @@ router.get("/csv/export", async (req, res, next) => {
   });
 });
 
+//PDF (NOT WORKING)
+
+// router.post("/checkout", async (req, res, next) => {
+//   // Define font files
+//   var fonts = {
+//     Roboto: {
+//       normal: "fonts/Roboto-Regular.ttf",
+//       bold: "fonts/Roboto-Medium.ttf",
+//       italics: "fonts/Roboto-Italic.ttf",
+//       bolditalics: "fonts/Roboto-MediumItalic.ttf",
+//     },
+//   };
+
+//   var PdfPrinter = require("pdfmake");
+//   var printer = new PdfPrinter(fonts);
+//   var fs = require("fs");
+
+//   var docDefinition = {
+//     // ...
+//   };
+
+//   var options = {
+//     // ...
+//   };
+
+//   var pdfDoc = printer.createPdfKitDocument(docDefinition, options);
+//   pdfDoc.pipe(fs.createWriteStream("document.pdf"));
+//   pdfDoc.end();
+// });
+
 //RETRIEVE
 router.get("/", async (req, res, next) => {
   res.send(await readFile(cartFilePath));
